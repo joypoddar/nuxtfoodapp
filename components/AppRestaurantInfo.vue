8224e2eae560eb4f2f6838f4429c58f75c520666
Joy Poddar <joypoddar.sd@gmail.com>
2021-02-14 19:53:05 +0530

    style: spacing

--- components/AppRestaurantInfo.vue
+++ components/AppRestaurantInfo.vue
@@ -2,8 +2,8 @@
   <section class="restaurantinfo">
     <div v-for="store in fooddata" :key="store.id">
       <h2>{{ store.name }}</h2>
-      <p>Delivery Time{{ store.deliveryTime }}</p>
-      <p>Rating {{ store.rating }}</p>
+      <p>Delivery Time: {{ store.deliveryTime }} mins</p>
+      <p>Rating: {{ store.rating }}</p>
       <p v-if="store.freeDelivery" class="label">
         <span>Free Delivery</span>
       </p>
