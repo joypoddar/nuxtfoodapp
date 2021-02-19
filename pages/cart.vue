421071f445742cae13f7b9a4d5d02e084ec9380a
Joy Poddar <joypoddar.sd@gmail.com>
2021-02-19 14:03:13 +0530

    vuex store shopping cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,12 +1,48 @@
 <template>
-  <main class="container cart">
-    <h2>Cart</h2>
+  <main class="cart container">
+    <h2>Your Cart</h2>
+
+    <section v-if="cart.length">
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Add Ons</th>
+            <th>Amount</th>
+            <th>Total Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in cart" :key="item.id">
+            <td>
+              {{ item.item }}
+              <span v-if="item.options">- {{ item.options }}</span>
+            </td>
+            <td>
+              <span v-for="addon in item.addOns" :key="addon" class="comma">{{
+                addon
+              }}</span>
+            </td>
+            <td>{{ item.count }}</td>
+            <td>{{ item.combinedPrice }}</td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td class="total">Total: ${{ totalPrice.toFixed(2) }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
   </main>
 </template>
 
 <script>
-export default {};
-</script>
+import { mapState, mapGetters } from "vuex";
 
-<style lang="scss" scoped>
-</style>
+export default {
+  computed: {
+    ...mapState(["cart"]),
+    ...mapGetters(["totalPrice"]),
+  },
+};
+</script>
